0917cd5a34f2de70561bfdc57362a59177894f46
PravallikaNagesh <pravallikanagesh.b@gmail.com>
2016-11-15 13:13:33 +0530

    minor changes

--- examples/coapserver/coapserver.ino
+++ examples/coapserver/coapserver.ino
@@ -1,107 +1,100 @@
-#include <SPI.h>
-#include <Dhcp.h>
-#include <Dns.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
+
+#include <ESP8266WiFi.h>
 #include <coap.h>
-
-byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
-
-// CoAP client response callback
-void callback_response(CoapPacket &packet, IPAddress ip, int port);
 
 // CoAP server endpoint url callback
 void callback_light(CoapPacket &packet, IPAddress ip, int port);
 
-// UDP and CoAP class
-EthernetUDP Udp;
-Coap coap(Udp);
+
+Coap coap;
+
+const char* ssid = "IOT";
+const char* password = "hasiot123";
 
 // LED STATE
 bool LEDSTATE;
 
+
 // CoAP server endpoint URL
-void callback_light(CoapPacket &packet, IPAddress ip, int port) {
+void callback_light(CoapPacket *packet, IPAddress ip, int port) {
   Serial.println("[Light] ON/OFF");
-  
+
   // send response
-  char p[packet.payloadlen + 1];
-  memcpy(p, packet.payload, packet.payloadlen);
-  p[packet.payloadlen] = NULL;
-  
+  char p[packet->payloadlen + 1];
+  memcpy(p, packet->payload, packet->payloadlen);
+  p[packet->payloadlen] = NULL;
+
   String message(p);
 
   if (message.equals("0"))
     LEDSTATE = false;
-  else if(message.equals("1"))
+  else if (message.equals("1"))
     LEDSTATE = true;
-      
+
   if (LEDSTATE) {
-    digitalWrite(9, HIGH) ; 
-    coap.sendResponse(ip, port, packet.messageid, "1");
-  } else { 
-    digitalWrite(9, LOW) ; 
-    coap.sendResponse(ip, port, packet.messageid, "0");
+    digitalWrite(16, HIGH) ;
+   coap.sendResponse(packet, ip, port, "1");
+  } else {
+    digitalWrite(16, LOW) ;
+    coap.sendResponse(packet, ip, port, "0");
   }
 }
 
-// CoAP client response callback
-void callback_response(CoapPacket &packet, IPAddress ip, int port) {
-  Serial.println("[Coap Response got]");
-  
-  char p[packet.payloadlen + 1];
-  memcpy(p, packet.payload, packet.payloadlen);
-  p[packet.payloadlen] = NULL;
-  
-  Serial.println(p);
-}
 
 void setup() {
-  Serial.begin(9600);
+  yield();
+  Serial.begin(115200);
 
-  Ethernet.begin(mac);
-  Serial.print("My IP address: ");
-  for (byte thisByte = 0; thisByte < 4; thisByte++) {
-    Serial.print(Ethernet.localIP()[thisByte], DEC);
+  WiFi.begin(ssid, password);
+  Serial.println(" ");
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    //delay(500);
+    yield();
     Serial.print(".");
   }
-  Serial.println();
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
 
   // LED State
-  pinMode(9, OUTPUT);
-  digitalWrite(9, HIGH);
+  pinMode(16, OUTPUT);
+  digitalWrite(16, HIGH);
   LEDSTATE = true;
-  
+
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+  //LEDSTATE = true;
+
+
   // add server url endpoints.
   // can add multiple endpoint urls.
   // exp) coap.server(callback_switch, "switch");
   //      coap.server(callback_env, "env/temp");
   //      coap.server(callback_env, "env/humidity");
-  Serial.println("Setup Callback Light");
+ 
   coap.server(callback_light, "light");
+  coap.server(callback_lightled, "lightled");
 
-  // client response callback.
-  // this endpoint is single callback.
-  Serial.println("Setup Response Callback");
-  coap.response(callback_response);
 
   // start coap server/client
   coap.start();
+ // coap.start(5683);
 }
 
 void loop() {
-  // send GET or PUT coap request to CoAP server.
-  // To test, use libcoap, microcoap server...etc
-  // int msgid = coap.put(IPAddress(10, 0, 0, 1), 5683, "light", "1");
-  Serial.println("Send Request");
-  int msgid = coap.get(IPAddress(XXX, XXX, XXX, XXX), 5683, "time");
-
-  delay(1000);
   coap.loop();
+  delay(1000);
 }
 /*
-if you change LED, req/res test with coap-client(libcoap), run following.
-coap-client -m get coap://(arduino ip addr)/light
-coap-client -e "1" -m put coap://(arduino ip addr)/light
-coap-client -e "0" -m put coap://(arduino ip addr)/light
+  if you change LED, req/res test with coap-client(libcoap), run following.
+  coap-client -m get coap://(arduino ip addr)/light
+  coap-client -e "1" -m put coap://(arduino ip addr)/light
+  coap-client -e "0" -m put coap://(arduino ip addr)/light
 */
