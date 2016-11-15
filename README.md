65191cb5bf9a29307e5a1f1c4eb92560cabaabde
PoornimaNagesh <pravallikanagesh.b@gmail.com>
2016-11-15 18:50:09 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
-# ESP-CoAP
-This repo contains CoAP protocol for operating ESP-12E as coap server. 
+# ESP-CoAP server library for Arduino
+This repo contains CoAP protocol for operationg ESP-12E as server.
 
 # CoAP server library for Arduino.
-<a href="https://github.com/automote/ESP-CoAP" simple server library for Arduino.
+<a href="http://coap.technology/" target=_blank>CoAP</a> simple server library for Arduino.
 
 ## Source Code
 This lightweight library source code are only 2 files. coap.cpp, coap.h.
 
 ## Example
 Some sample sketches for Arduino included(/examples/).
- 
- - coapserver.ino :simple server with endpoint url callback sample.
+
+- coapserver.ino :simple server endpoint url callback sample.
 
 ## How to use
-Download this source code branch zip file and extract to the Arduino libraries directory or checkout repository.
+Download this source code branch zip file and extract to the Arduino libraries directory or checkout repository. Here is checkout on Ubuntu.
 
-    cd $HOME/Documents/Arduino/libraries/
+    cd $HOME/Downloads/Arduino/libraries/
     git clone https://github.com/automote/ESP-CoAP
     # restart Arduino IDE, you can find ESP-CoAP examples.
-Upload the example code to ESP-12E,get the IPaddress of ESP-12E(server),run the coap client with following URI coap://IPaddress:default port number/resource to connect to ESP_12E server
 
+-Upload the example code to ESP-12E,get the IPaddress of ESP-12E(server).
+-Run the coap client with following URI coap://IPaddress:default port number/resource ,to connect to ESP_12E server.
 
+For more information about this library please vist <a href="https://github.com/automote/ESP-CoAP">here</a>.
---
+++ coapserver.ino
@@ -0,0 +1,100 @@
+
+#include <ESP8266WiFi.h>
+#include <coap.h>
+
+// CoAP server endpoint url callback
+void callback_light(CoapPacket &packet, IPAddress ip, int port);
+
+
+Coap coap;
+
+const char* ssid = "IOT";
+const char* password = "hasiot123";
+
+// LED STATE
+bool LEDSTATE;
+
+
+// CoAP server endpoint URL
+void callback_light(CoapPacket *packet, IPAddress ip, int port) {
+  Serial.println("[Light] ON/OFF");
+
+  // send response
+  char p[packet->payloadlen + 1];
+  memcpy(p, packet->payload, packet->payloadlen);
+  p[packet->payloadlen] = NULL;
+
+  String message(p);
+
+  if (message.equals("0"))
+    LEDSTATE = false;
+  else if (message.equals("1"))
+    LEDSTATE = true;
+
+  if (LEDSTATE) {
+    digitalWrite(16, HIGH) ;
+   coap.sendResponse(packet, ip, port, "1");
+  } else {
+    digitalWrite(16, LOW) ;
+    coap.sendResponse(packet, ip, port, "0");
+  }
+}
+
+
+void setup() {
+  yield();
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println(" ");
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    //delay(500);
+    yield();
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+  // LED State
+  pinMode(16, OUTPUT);
+  digitalWrite(16, HIGH);
+  LEDSTATE = true;
+
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+  //LEDSTATE = true;
+
+
+  // add server url endpoints.
+  // can add multiple endpoint urls.
+  // exp) coap.server(callback_switch, "switch");
+  //      coap.server(callback_env, "env/temp");
+  //      coap.server(callback_env, "env/humidity");
+ 
+  coap.server(callback_light, "light");
+  
+
+
+  // start coap server/client
+  coap.start();
+ // coap.start(5683);
+}
+
+void loop() {
+  coap.loop();
+  delay(1000);
+}
+/*
+  if you change LED, req/res test with coap-client(libcoap), run following.
+  coap-client -m get coap://(arduino ip addr)/light
+  coap-client -e "1" -m put coap://(arduino ip addr)/light
+  coap-client -e "0" -m put coap://(arduino ip addr)/light
+*/
